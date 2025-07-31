66322b435c326af4ea9fa37538e229f930b5ee66
satyamraj48 <satyamraj48@gmail.com>
2025-07-31 14:50:30 +0530

    nodemailer update

--- SERVER/utils/mailSender.js
+++ SERVER/utils/mailSender.js
@@ -4,8 +4,6 @@ const mailSender = async (email, title, body) => {
 	try {
 		const transporter = nodemailer.createTransport({
 			host: process.env.MAIL_HOST,
-			port: 587,
-			secure:false,
 			auth: {
 				user: process.env.MAIL_USER,
 				pass: process.env.MAIL_PASS,
